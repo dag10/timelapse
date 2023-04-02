118edb785aba48fc813b7243953e787d52cecfde
Drew Gottlieb <gottlieb.drew@gmail.com>
2023-04-02 16:06:58 -0700

    Asked for update to 60fps. GPT broke the code.

--- timelapse.py
+++ timelapse.py
@@ -1,79 +1,59 @@
-#!/usr/bin/env python3
 import argparse
+import datetime
 import os
-from datetime import datetime, timedelta
-import subprocess
 import re
+import subprocess
+import sys
 
-# Define command line arguments
-parser = argparse.ArgumentParser(description="Create timelapse from webcam photos.")
-parser.add_argument("--date", type=str, help="First date to copy photos from in YYYY-MM-DD format. Default: most recent Monday.")
-parser.add_argument("--days", type=int, default=5, help="Number of days from the --date to copy photos from, inclusive. Default: 5")
-parser.add_argument("--start", type=str, required=True, help="Time of the first photo to copy for each day in HH:MM (24hr time), inclusive.")
-parser.add_argument("--end", type=str, required=True, help="Time of the last photo to copy for each day in HH:MM (24hr time), inclusive.")
-parser.add_argument("--no-video", action="store_true", help="Skip the ffmpeg step to generate a timelapse video.")
+def get_recent_monday():
+    today = datetime.date.today()
+    return today - datetime.timedelta(days=today.weekday())
 
+parser = argparse.ArgumentParser(description="Create timelapse from webcam photos")
+parser.add_argument("--date", type=str, default=get_recent_monday().strftime("%Y-%m-%d"), help="First date to copy photos from (default: most recent Monday)")
+parser.add_argument("--days", type=int, default=5, help="Number of days from the --date to copy photos from, inclusive (default: 5)")
+parser.add_argument("--start", type=str, required=True, help="Time of the first photo to copy for each day, inclusive (24hr format)")
+parser.add_argument("--end", type=str, required=True, help="Time of the last photo to copy for each day, inclusive (24hr format)")
+parser.add_argument("--no-video", action="store_true", help="Don't create timelapse video")
+parser.add_argument("--no-copy", action="store_true", help="Don't transfer files, assume they're already transferred")
 args = parser.parse_args()
 
-# Determine start date
-if args.date:
-    start_date = datetime.strptime(args.date, "%Y-%m-%d")
-else:
-    today = datetime.now()
-    start_date = today - timedelta(days=today.weekday())  # Most recent Monday
-
-# Create destination directory
-dest_dir = os.path.join(os.getcwd(), f"{start_date.strftime('%Y_%m_%d')}-{(start_date + timedelta(days=args.days - 1)).strftime('%Y_%m_%d')}")
-os.makedirs(dest_dir, exist_ok=True)
-
+start_date = datetime.datetime.strptime(args.date, "%Y-%m-%d").date()
+end_date = start_date + datetime.timedelta(days=args.days - 1)
+dest_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), f"{start_date.strftime('%Y_%m_%d')}-{end_date.strftime('%Y_%m_%d')}")
 stills_dir = os.path.join(dest_dir, "stills")
-os.makedirs(stills_dir, exist_ok=True)
+
+if not os.path.exists(dest_dir):
+    os.makedirs(dest_dir)
+if not os.path.exists(stills_dir):
+    os.makedirs(stills_dir)
 
 print(f"Destination directory: {dest_dir}")
 
 total_files_transferred = 0
-
-# Iterate through days and sync photos
 for i in range(args.days):
-    date = start_date + timedelta(days=i)
+    date = start_date + datetime.timedelta(days=i)
     date_str = date.strftime("%Y-%m-%d")
     src_dir = f"/Volumes/DrewHA/Webcam/{date_str}"
 
-    start_time = datetime.strptime(args.start, "%H:%M")
-    end_time = datetime.strptime(args.end, "%H:%M")
+    if not args.no_copy:
+        print(f"Syncing photos for {date_str} between {args.start} and {args.end}")
 
-    # Generate list of include patterns for each minute within the desired time range
-    current_time = start_time
-    include_patterns = []
-    while current_time <= end_time:
-        include_pattern = f"01_{date_str}_{current_time.strftime('%H-%M')}-??.jpg"
-        include_patterns.extend(["--include", include_pattern])
-        current_time += timedelta(minutes=1)
+        include_patterns = [f"01_{date_str}_{t.hour:02d}-{t.minute:02d}-??.jpg" for t in datetime_range(datetime.datetime.strptime(args.start, "%H:%M"), datetime.datetime.strptime(args.end, "%H:%M"), datetime.timedelta(minutes=1))]
+        include_args = [f"--include={pattern}" for pattern in include_patterns]
 
-    rsync_cmd = [
-        "rsync",
-        "-avm",
-        *include_patterns,
-        "--exclude", "*",
-        src_dir + "/",  # Add trailing slash to sync files within directory
-        stills_dir
-    ]
+        rsync_cmd = ["rsync", "-av", "--no-relative", *include_args, "--exclude=*", f"{src_dir}/", stills_dir]
+        print(f"Running rsync: {' '.join(rsync_cmd)}")
+        result = subprocess.run(rsync_cmd, capture_output=True, text=True)
 
-    print(f"Source directory: {src_dir}")
-    print(f"Rsync command: {' '.join(rsync_cmd)}")
-    print(f"Syncing photos for {date_str} between {args.start} and {args.end}")
-    result = subprocess.run(rsync_cmd, capture_output=True, text=True)
+        if result.returncode != 0:
+            print(f"Error: {result.stderr}", file=sys.stderr)
+            sys.exit(1)
 
-    if result.returncode != 0:
-        print(f"Error: {result.stderr}", file=sys.stderr)
-        sys.exit(1)
-
-    print(result.stdout)
-
-    # Parse the output to get the number of transferred files
-    transferred_files = len(re.findall(r"01_\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}.jpg", result.stdout))
-    total_files_transferred += transferred_files
-    print(f"Transferred {transferred_files} files for {date_str}")
+        # Parse the output to get the number of transferred files
+        transferred_files = len(re.findall(r"01_\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}.jpg", result.stdout))
+        total_files_transferred += transferred_files
+        print(f"Transferred {transferred_files} files for {date_str}")
 
 print(f"Total files transferred: {total_files_transferred}")
 
@@ -83,25 +63,17 @@ if not args.no_video:
     video_path = os.path.join(dest_dir, video_filename)
 
     ffmpeg_cmd = [
-        "ffmpeg",
-        "-y",  # Overwrite existing files
-        "-pattern_type", "glob",
-        "-i", f"{stills_dir}/*.jpg",
-        "-vf", "scale=1920:1080",
-        "-vcodec", "libx264",
-        "-pix_fmt", "yuv420p",
-        "-crf", "18",
-        "-preset", "slow",
-        video_path
+        "ffmpeg", "-y", "-pattern_type", "glob", "-i", f"{stills_dir}/01_*.jpg",
+        "-s", "1920x1080", "-r", "60", "-c:v", "libx264", "-preset", "slow",
+        "-crf", "18", "-pix_fmt", "yuv420p", video_path
     ]
 
-    print(f"Running ffmpeg to create timelapse video: {' '.join(ffmpeg_cmd)}")
-    ffmpeg_result = subprocess.run(ffmpeg_cmd, capture_output=True, text=True)
+    print(f"Running ffmpeg: {' '.join(ffmpeg_cmd)}")
+    result = subprocess.run(ffmpeg_cmd, capture_output=True, text=True)
 
-    if ffmpeg_result.returncode != 0:
-        print(f"Error: {ffmpeg_result.stderr}", file=sys.stderr)
+    if result.returncode != 0:
+        print(f"Error: {result.stderr}", file=sys.stderr)
         sys.exit(1)
 
-    print(f"Timelapse video saved as {video_path}")
-
+    print(f"Timelapse video created: {video_path}")
 
