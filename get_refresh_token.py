ee0bdbce61335a54edde6f41a6a54d3f164cebaa
Drew Gottlieb <gottlieb.drew@gmail.com>
2023-04-02 22:35:23 -0700

    Fixed automatic playlist adding on upload.

--- get_refresh_token.py
+++ get_refresh_token.py
@@ -1,11 +1,24 @@
 import os
 import json
+import tempfile
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 if __name__ == "__main__":
-    scopes = ["https://www.googleapis.com/auth/youtube.upload"]
-    flow = InstalledAppFlow.from_client_secrets_file("client_secret.json", scopes)
+    with open("client_secret.json", "r") as secrets_file:
+        secrets_data = json.load(secrets_file)
+
+    # Add the required scope
+    secrets_data["installed"]["scopes"] = ["https://www.googleapis.com/auth/youtube.force-ssl"]
+
+    # Save the updated secrets to a temporary file
+    with tempfile.NamedTemporaryFile(mode="w", delete=False) as temp_secrets_file:
+        json.dump(secrets_data, temp_secrets_file)
+        temp_secrets_file.flush()
+
+    # Get the credentials using the temporary file
+    scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
+    flow = InstalledAppFlow.from_client_secrets_file(temp_secrets_file.name, scopes)
     credentials = flow.run_local_server(port=0)
 
     token_data = {
@@ -19,4 +32,7 @@ if __name__ == "__main__":
         json.dump(token_data, config_file)
 
     print("Refresh token saved to config.json")
+
+    # Remove the temporary file
+    os.unlink(temp_secrets_file.name)
 
--- prompt.md
+++ prompt.md
@@ -1,1 +1,28 @@
-Have the function and script accept an optional fourth arg, `playlist_id`, which is a string of a playlist ID to add the video to automatically upon uploading. For example, "PLnZFyIYD4hEnHwQw7_4GVGhtO-Qk8iXtt". 
+Modify the `get_refresh_token.py` script to do all this for us, adding the scope and all. Have it accept a default `client_secrets.json` file (no scopes manually added) and have it add the scopes needed to that file before running the credentials flow.
+
+For your reference, here's the existing get_refresh_token.py:
+
+```
+import os
+import json
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+if __name__ == "__main__":
+    scopes = ["https://www.googleapis.com/auth/youtube.upload"]
+    flow = InstalledAppFlow.from_client_secrets_file("client_secret.json", scopes)
+    credentials = flow.run_local_server(port=0)
+
+    token_data = {
+        "client_id": credentials.client_id,
+        "client_secret": credentials.client_secret,
+        "refresh_token": credentials.refresh_token,
+        "type": "authorized_user"
+    }
+
+    with open("config.json", "w") as config_file:
+        json.dump(token_data, config_file)
+
+    print("Refresh token saved to config.json")
+```
+
